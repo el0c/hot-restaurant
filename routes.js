a22b06bd12d65d486ea23a2229fe8aadb9dedc74
el0c <36245622+el0c@users.noreply.github.com>
2019-03-16 12:34:54 -0500

    Small fix in routes to point to tables

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "./index.html"));
     });
 
-    app.get("/table", function(req, res) {
+    app.get("/tables", function(req, res) {
         res.sendFile(path.join(__dirname, "./tables.html"));
     });
 
